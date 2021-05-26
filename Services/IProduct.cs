0c357b78c0a148b8b2f439c8541c81f2b6eeb211
Geraldo <geraldovilela19@gmail.com>
2021-05-25 20:28:20 -0400

    dev: adicionado Interface do CRUD de produtos.

---
+++ Services/IProduct.cs
@@ -0,0 +1,22 @@
+﻿using comics.api.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comics.api.Services
+{
+    interface IProduct
+    {
+        public Product Create(Product product);
+
+        public Product Update(Product productChanges);
+        public List<Product> GetAll();
+
+        public Product Get(string guid);
+
+        public void Delete(string guid);
+
+
+    }
+}
